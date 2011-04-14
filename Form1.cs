4771bf98688736c7d546fc7ea81c9bb9484ae5f7
Styder <styderddd@gmail.com>
2011-04-14 15:53:47 +0400

    Auth interface added.

--- Form1.cs
+++ Form1.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace Market_Management
 {
-	public partial class Form1 : Form
+	public partial class AuthForm : Form
 	{
-		public Form1()
+		public AuthForm()
 		{
 			InitializeComponent();
 		}
--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace Market_Management
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new AuthForm());
 		}
 	}
 }
